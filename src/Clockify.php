4a69dc3fc5f5a71b1feeca9441da2e944a35cfc3
Markus Martens <m.martens@web-netz.de>
2022-09-27 12:05:19 +0200

    🧙 update

--- src/Clockify.php
+++ src/Clockify.php
@@ -2,17 +2,58 @@
 
 namespace m4rku5\Clockify;
 
+use RuntimeException;
+
 class Clockify
 {
-    /** @var array $config */
-    private $config;
+    /** @var string $baseurl */
+    protected string $baseurl;
+    /** @var string $apikey */
+    protected string $apikey;
 
-    public function __construct()
+    public function __construct(string $baseurl, string $apikey)
     {
+        $this->baseurl = $baseurl;
+        $this->apikey = $apikey;
     }
 
-    public function get(): string
+    private function _curl(string $url, array $options = []): array
     {
-        return '';
+        $ch = curl_init($url);
+        curl_setopt_array($ch, array_replace_recursive([
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_HTTPHEADER     => [
+                "X-Api-Key: " . $this->apikey,
+                "Content-Type: application/json",
+            ],
+        ], $options));
+        $exec = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($exec, true);
+    }
+
+    public function GET(string $endpoint): array
+    {
+        return $this->_curl($this->baseurl . '/' . $endpoint, [CURLOPT_HTTPGET => true]);
+    }
+
+    public function PUT()
+    {
+        throw new RuntimeException('not yet implemented');
+    }
+
+    public function POST(string $endpoint, array $data): array
+    {
+        return $this->_curl($this->baseurl . '/' . $endpoint, [
+            CURLOPT_POST       => true,
+            CURLOPT_POSTFIELDS => $data,
+        ]);
+    }
+
+    public function user()
+    {
+        return $this->GET('user');
     }
 }
